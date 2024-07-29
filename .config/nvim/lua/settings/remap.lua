0a27eba6f0a3d19475402fe24ba301eeeffc116a
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-07-29 13:58:39 +0200

    fix visual mode multi-line move up remap keybinding

--- .config/nvim/lua/settings/remap.lua
+++ .config/nvim/lua/settings/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Beautiful line(s) movement in visal mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Yanks to clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
