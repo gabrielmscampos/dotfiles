6b618256d382df712250eb8fa13626da85bd3204
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-11-07 23:26:40 +0100

    add <leader>fr keymap to open lsp references in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,10 +9,14 @@ return {
         local ps = function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end
+        local fr = function()
+            require('telescope.builtin').lsp_references()
+        end
 
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>ps', ps)
+        vim.keymap.set('n', '<leader>fr', fr, { noremap = true, silent = true })
     end
 }
