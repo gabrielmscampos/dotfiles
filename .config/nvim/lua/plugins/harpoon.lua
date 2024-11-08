a3e07141f0fce7c51c298267b3ebb8e59d24e792
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-11-08 12:44:19 +0100

    update harpoon keymaps and add prev and next keymappings

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -18,13 +18,16 @@ return {
         vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end, { desc = "Harpoon: select 2" })
         vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end, { desc = "Harpoon: select 3" })
         vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end, { desc = "Harpoon: select 4" })
-        vim.keymap.set("n", "<leader><C-1>", function() harpoon:list():replace_at(1) end,
+        vim.keymap.set("n", "<C-S-1>", function() harpoon:list():replace_at(1) end,
             { desc = "Harpoon: replace at 1" })
-        vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end,
+        vim.keymap.set("n", "<C-S-2>", function() harpoon:list():replace_at(2) end,
             { desc = "Harpoon: replace at 2" })
-        vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end,
+        vim.keymap.set("n", "<C-S-3>", function() harpoon:list():replace_at(3) end,
             { desc = "Harpoon: replace at 3" })
-        vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end,
+        vim.keymap.set("n", "<C-S-4>", function() harpoon:list():replace_at(4) end,
             { desc = "Harpoon: replace at 4" })
+
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon: go to previous buffer" })
+        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Harpoon: go to next buffer" })
     end
 }
