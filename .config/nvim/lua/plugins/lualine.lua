49b5ec52b7db1da66c802ac96c361e1fa20855c3
Gabriel Moreira <gabrielmscampos@gmail.com>
2026-01-20 10:20:58 +0100

    feat: add file status and path to lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,16 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("lualine").setup()
+        require("lualine").setup({
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        file_status = true,
+                        path = 1,
+                    }
+                }
+            }
+        })
     end
 }
